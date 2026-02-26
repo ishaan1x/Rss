120cf562a7804c52a0e47c326d468cd21802f04d
ishaan1x <ishaankumadm@gmail.com>
2026-02-26 13:27:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from telegram import Bot
 
 # Configuration
-BOT_TOKEN = "8397682707:AAGyTrnumyd3TIu8lfErnbZoOdXAVRUUt_0"
-CHAT_ID = "-1003208146639"
+BOT_TOKEN = "8620564950:AAEegBh58f0VQpX1IeEYMdZn2zl86X4p4xA"
+CHAT_ID = "-1003845689752"
 
 # Initialize Telegram bot
 bot = Bot(token=BOT_TOKEN)
